4cce766522f622af9451d5c0dc22adbee8dc3e5c
remy <remy.forjanic@gmail.com>
2022-11-30 11:20:13 +0100

    source & include folder

---
+++ include/test.h
@@ -0,0 +1,10 @@
+//
+// Created by remy on 11/30/22.
+//
+
+#ifndef YOPE_TEST_H
+#define YOPE_TEST_H
+
+void print_hello();
+
+#endif //YOPE_TEST_H
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by remy on 11/30/22.
+//
+
+#include "test.h"
+#include <iostream>
+
+void print_hello() {
+    std::cout << "Hello, world!\n" << std::endl;
+}
