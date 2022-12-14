f1ba4cf2d1c17902e94d5c101a1cdf1658b3a516
Mexigore <axelc2000@live.com.mx>
2022-12-14 17:21:00 +0100

    All classes declared and some implemented

--- include/node.h
+++ include/node.h
@@ -4,6 +4,11 @@
 
 #ifndef YOPE_NODE_H
 #define YOPE_NODE_H
+
+#include <string>
+#include <memory>
+#include <vector>
+
 enum NodeType {
     VALUE, //store value type
     BIN_OP,
@@ -24,26 +29,153 @@ enum NodeType {
 };
 
 enum OpType {
+    SUM,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MAX,
+    MIN,
+    MOD,
+    INCREMENT,
+    DECREMENT,
+    AND,
+    OR,
+    XOR,
+    NOT,
+    EQUAL
+};
 
+enum ValueType {
+    NULLL,
+    STRING,
+    INT,
+    FLOAT,
+    BOOL,
+    ARRAY
 };
 
 class INode {
 public:
     NodeType m_type;
     INode(NodeType type);
-    std::string to_string;
+    virtual std::string to_string() = 0;
 };
 
 class BinOpNode : public INode {
 public:
-    INode lhs;
-    OpType op;
-    INode rhs;
+    BinOpNode(std::shared_ptr<INode>  lhs, OpType op, std::shared_ptr<INode>  rhs);
+    std::string to_string() override;
+
+    std::shared_ptr<INode> m_lhs;
+    OpType m_op;
+    std::shared_ptr<INode> m_rhs;
 };
+
+class UnOpNode : public INode {
+public:
+    UnOpNode(std::shared_ptr<INode> lhs, OpType);
+    std::string to_string() override;
+
+    std::shared_ptr<INode> m_lhs;
+    OpType m_op;
+};
+
+class IfNode : public INode {
+public:
+    IfNode(std::shared_ptr<INode> );
+    std::string to_string() override;
+
+
+};
+
+class FunDefNode : public INode {
+public:
+    FunDefNode();
+    std::string to_string() override;
+
+    std::string m_name;
+    ValueType m_return_type;
+    std::vector<std::shared_ptr<INode> > m_params;
+    std::vector<INode> m_body;
+};
+
+class FunCallNode : public INode {
+public:
+    FunCallNode();
+    std::string to_string() override;
+
+    std::string m_name;
+    std::shared_ptr<std::shared_ptr<INode>> m_params;
+};
+
+class VarDefNode : public INode {
+public:
+    VarDefNode();
+    std::string to_string() override;
+
+    ValueType m_type;
+    std::string m_name;
+    std::shared_ptr<INode> m_value;
+};
+
+class VarCallNode : public INode {
+public:
+    VarCallNode();
+    std::string to_string() override;
+
+    std::string m_name;
+};
+
+class VarAssNode : public INode {
+    VarAssNode();
+    std::string to_string() override;
+
+    std::string m_name;
+    std::shared_ptr<INode> m_new_value;
+};
+
+class ForLoopNode : public INode {
+    ForLoopNode();
+    std::string to_string() override;
+
+};
+
+class WhileLoopNode : public INode {
+    WhileLoopNode();
+    std::string to_string() override;
+
+};
+
+class IncludeNode : public INode {
+    IncludeNode();
+    std::string to_string() override;;
+
+    std::string m_path;
+};
+
+class BreakNode : public INode {
+    BreakNode();
+    std::string to_string() override;
+};
+
+class ReturnNode : public INode {
+    ReturnNode();
+    std::string to_string() override;
+
+    ValueType m_return_type;
+};
+
+class InterruptNode : public INode {
+
+};
+
+class SwitchNode : public INode {
+
+};
+
 
 class node {
 
 };
-
 
 #endif //YOPE_NODE_H
--- src/node.cpp
+++ src/node.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "node.h"
+
+BinOpNode::BinOpNode(std::shared_ptr<INode> lhs, OpType op, std::shared_ptr<INode> rhs):
+INode(NodeType::BIN_OP),
+m_lhs{lhs},
+m_op{op},
+m_rhs{rhs}
+{
+
+}
+
+std::string BinOpNode::to_string() {
+    return std::string();
+}
+
+INode::INode(NodeType type): m_type(type) {
+
+}
